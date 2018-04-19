5f499d98b27565e82790279cf85afba8089a649b
Tirso Reyes <tirso.reyes.hdez@gmail.com>
2018-04-18 22:53:59 -0500

    Initial commit

---
+++ src/main/java/edu/itssmt/appwebfish/AppwebfishApplication.java
@@ -0,0 +1,12 @@
+package edu.itssmt.appwebfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwebfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppwebfishApplication.class, args);
+	}
+}
